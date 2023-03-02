d85b81f7a40aa726d4ba9e76355d26af93fa6caf
Elizaveta Guseva <liza.guseva@hey.com>
2023-03-03 00:24:31 +0100

    pretty sad text-area

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [myTxt, setMyTxt] = useState('')
+
+  const handleTextArea = (e) => {
+    setMyTxt(myTxt)
+    console.log(e.target.value)
+  }
   return (
     <div className="App">
       <div className='Body'>
         <h1>My pretty sad journal</h1>
         <div className='page'>
-        <p>
-          I'm a sad person. I have sad logs. Very sad logs. 
-          </p>
-          <p id='poem'>
-          The weight of sorrow, heavy and deep
-          A shadow that never seems to sleep
-          Heartache that pierces like a knife
-          Pain that consumes my every life
-          </p>
-          <p>
-          Memories of what once was bright
-          Now faded and lost in the night
-          The emptiness that fills my soul
-          Aching for something to make me whole
-          </p>
-          <p>
-          Oh, how I long to be free
-          From this sadness that's consuming me
-          But for now, I'll bear this heavy load
-          And hope that someday, I'll find the road.
-        </p>
+          <textarea 
+            id="textarea1" 
+            class="input shadow" 
+            name="name" 
+            rows="15" 
+            cols="100" 
+            placeholder="Your text here "
+            onInput={handleTextArea}>
+          </textarea>
         </div>
-        <div className='page'>
-          </div>
       </div>
     </div>
   );
